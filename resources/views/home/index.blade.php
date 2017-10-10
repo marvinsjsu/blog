22fce29fe4b2d875aaf4886713fb11e2486b9a9b
Marvin Mante <mmante@sugarcrm.com>
2017-10-09 19:08:38 -0700

    adding all files

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GrownBy: - Cultivation and community for Cannibis</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link async href="http://fonts.googleapis.com/css?family=Battambang" data-generated="http://enjoycss.com" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="/css/main.css" />
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.8.0/slick.css"/>
+</head>
+<body class="index">
+<div id="page-wrapper">
+    <!-- Header -->
+    <header id="header" class="alt">
+        <h1 id="logo"><a href="/home">GrownBy <span> - Let's get cultivating</span></a></h1>
+        <nav id="nav">
+            <ul>
+                <li><a href="#banner" class="scrolly">Home</a></li>
+                <li><a href="#about-us" class="scrolly">About Us</a></li>
+                <li><a href="/blog" class="scrolly">Blog</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section id="banner">
+        <div class="inner">
+            <br/>
+            <br/>
+            <header>
+                <h2>GrownBy</h2>
+            </header>
+            <p>
+                organic marijuana <br/>
+                through sustainable means <br/>
+                &amp; <br/>
+                a community of artisan growers
+            </p>
+            <footer>
+                <br/>
+                <ul class="buttons vertical">
+                    <li><a href="/blog" class="button medium fit">Our journey in cultivating</a></li>
+                </ul>
+            </footer>
+        </div>
+    </section>
+
+    <article id="about-us">
+        <header class="special container">
+            <!-- <span class="icon fa-bar-chart-o"></span> -->
+            <div class="12u 12u">
+                <p>
+                    <!--[-->We are artisans from <!--]--> <strong><!--[-->Hollister<!--]--></strong>, <!--[--> California.<!--]-->
+                </p>
+                <p align="left">
+                <!--[-->Our interest in <strong><!--[-->growing organic marijuana through sustainable means<!--]--></strong> drives our journey.<!--]-->
+                    As we grow our plants, we want to <strong><!--[-->openly share ideas on all topics related to Cannibis<!--]--></strong>.  The immediate goal
+                    is to <strong><!--[-->share knowledge in techniques and tools of cultivation<!--]--></strong>, but the ultimate goal is to dispel any
+                    remaining negative beliefs or myths this plant has and help <strong><!--[-->cultivate a healthy community<!--]--></strong>
+                    that acknowledges, appreciates, and <strong><!--[-->experiences the many benefits of this plant<!--]--></strong>.
+                </p>
+            </div>
+            <section class="wrapper style2 container special-alt">
+                <div class="row 50%">
+                    <div class="12u 12u(narrower)">
+                        {{--<ul class="">--}}
+                            {{--<li><img src="images/banner_logo.jpg" width="30%" height="30%"/></li>--}}
+                            {{--<li><img src="images/banner_logo.jpg" width="30%" height="30%"/></li>--}}
+                            {{--<li><img src="images/banner_logo.jpg" width="30%" height="30%"/></li>--}}
+                        {{--</ul>--}}
+
+                        <ul class="buttons center">
+                            <li><a href="#" class="button medium fit">Photos and Video Gallery</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </header>
+    </article>
+
+    <section id="cta">
+        <p><!--[-->This is our exploration in the cultivation of various Cannabis strains<!--]--></p>
+        <ul class="icons">
+            <li><a href="#" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon circle fa-google-plus"><span class="label">Google+</span></a></li>
+            {{--<li><a href="#" class="icon circle fa-instagram"><span class="label">Instagram</span></a></li>--}}
+        </ul>
+        <h3><!--[-->Let's get <strong>Cultivating!</strong><!--]--></h3>
+    </section>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <ul class="copyright">
+            <li>&copy; GrownBy, LLC. 2017</li>
+        </ul>
+    </footer>
+</div>
+
+<!-- Scripts -->
+<script src="/js/jquery.min.js"></script>
+<script src="/js/jquery.dropotron.min.js"></script>
+<script src="/js/jquery.scrolly.min.js"></script>
+<script src="/js/jquery.scrollgress.min.js"></script>
+<script src="/js/skel.min.js"></script>
+<script src="/js/util.js"></script>
+<!--[if lte IE 8]><script src="public/js/ie/respond.min.js"></script><![endif]-->
+<script src="/js/main.js"></script>
+
+</body>
+</html>
